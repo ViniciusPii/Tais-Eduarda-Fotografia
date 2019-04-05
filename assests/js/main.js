cf56a780c7faab9c89079c9b5a5a83f197a5b111
Vinicius Sales <vinicius.n.sales@gmail.com>
2019-04-05 01:11:37 -0300

    adicionado

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,4 +1,8 @@
-$('menu a').click(function(e){
-    e.preventDefault();
-    var id = $
-});
+function menuClick(id) {
+    const element = document.getElementById(id);
+    const y = element.getBoundingClientRect().top + window.scrollY - 120;
+    window.scroll({
+        top: y,
+        behavior: 'smooth'
+    });
+}
