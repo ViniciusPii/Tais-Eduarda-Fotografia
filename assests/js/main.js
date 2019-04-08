967a9b0a67dcb0c8a52379c05883325d14e221d7
Vinicius Sales <vinicius.n.sales@gmail.com>
2019-04-08 12:08:35 -0300

    adicionado

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,6 +1,6 @@
 function menuClick(id) {
     const element = document.getElementById(id);
-    const y = element.getBoundingClientRect().top + window.scrollY - 120;
+    const y = element.getBoundingClientRect().top + window.scrollY - 50;
     window.scroll({
         top: y,
         behavior: 'smooth'
@@ -21,6 +21,48 @@ function mobileMenuClick(){
     } else {
       document.querySelector("#ulmobile").style.display = 'flex';
     }
+}
+
+
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+  
+  //smoothscroll
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+      
+      
+      
+      $('a').each(function () {
+          $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+    
+      var target = this.hash,
+          menu = target;
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top+2-120
+      }, 800, 'swing', function () {
+          $(document).on("scroll", onScroll);
+      });
+  });
+});
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('#menu-center a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top-120 <= scrollPos && refElement.position().top-70 + refElement.height() > scrollPos) {
+          $('#menu-center ul li a').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+  });
 }
 
 
